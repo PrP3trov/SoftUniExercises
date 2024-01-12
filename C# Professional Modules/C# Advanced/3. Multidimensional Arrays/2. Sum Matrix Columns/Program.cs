2ae7b2a21f3a0abf830fefc3c357300831f317d3
Preslav Petrov <tsrlegend06@gmail.com>
2024-01-12 21:46:47 +0200

    Added Multidimensional Arrays Exercise Solutions

---
+++ C# Professional Modules/C# Advanced/3. Multidimensional Arrays/2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+int rows = input[0];
+int cols = input[1];
+
+int[,] matrix = ReadMatrix(rows, cols, " ");
+
+
+for (int col = 0; col < matrix.GetLength(1); col++)
+{
+    int sum = 0;
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        sum += matrix[row,col];   
+    }
+    Console.WriteLine(sum);
+
+}
+
+
+int[,] ReadMatrix(int rows, int cols, string separator)
+{
+    int[,] matrix = new int[rows, cols];
+
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        int[] rowArray = Console.ReadLine().Split(separator).Select(int.Parse).ToArray();
+
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            matrix[row, col] = rowArray[col];
+        }
+    }
+    return matrix;
+}
---
+++ C# Professional Modules/C# Advanced/3. Multidimensional Arrays/3. Primary Diagonal/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[,] matrix = new int[n,n];
+int sum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int[] arayRow = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i,j] = arayRow[j];
+    }
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    sum += matrix[i,i];
+}
+Console.WriteLine(sum);
