a9cbdb0332673f3279fe0f261c3bf3be20a99a02
Preslav Petrov <tsrlegend06@gmail.com>
2024-01-13 16:40:46 +0200

    Added More Exercise Solutions

--- C# Professional Modules/C# Advanced/3. Multidimensional Arrays/4. Symbol in Matrix/Program.cs
+++ C# Professional Modules/C# Advanced/3. Multidimensional Arrays/4. Symbol in Matrix/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿int rows = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[rows, rows];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    char[] row = Console.ReadLine().ToArray();
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = row[j];
+    }
+}
+char symbol = char.Parse(Console.ReadLine());
+int check = (int)(symbol);
+int symbolRow = -1;
+int symbolCol = -1;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (matrix[i,j] == check)
+        {
+            symbolRow = i;
+            symbolCol = j;
+            check = int.MinValue;
+        }
+    }
+}
+if (symbolRow < 0 || symbolCol < 0)
+{
+    Console.WriteLine($"{symbol} does not occur in the matrix");
+}
+else
+{
+    Console.WriteLine($"({symbolRow}, {symbolCol})");
+}
