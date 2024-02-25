09af1b658da9e9468f0954b584ee9007d18e20ae
Preslav Petrov <tsrlegend06@gmail.com>
2024-02-25 14:29:42 +0200

    Added Encapsilation

---
+++ C# Professional Modules/C# OOP/03. Encapsilation/Salary/Person.cs
@@ -0,0 +1,34 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                Salary += Salary * ((percentage / 100) / 2);
+            }
+            else
+            {
+                Salary += Salary * (percentage / 100);
+            }
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
---
+++ C# Professional Modules/C# OOP/03. Encapsilation/Team/Person.cs
@@ -0,0 +1,78 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (age < 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get { return salary; }
+           private set
+            {
+                salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                Salary += Salary * ((percentage / 100) / 2);
+            }
+            else
+            {
+                Salary += Salary * (percentage / 100);
+            }
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
