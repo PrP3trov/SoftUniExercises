0265074262ee150e3322c3dad64498fb1ba0eb9e
Preslav Petrov <tsrlegend06@gmail.com>
2024-02-28 18:47:32 +0200

    Added Interfaces and Abstraction

---
+++ C# Professional Modules/C# OOP/05. Interfaces and Abstraction/Cars/Tesla.cs
@@ -0,0 +1,30 @@
+﻿namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{Color} {GetType().Name} {Model} with {Battery} Batteries \n{Start()} \n{Stop()}";
+        }
+    }
+}
